12e34374ee44d614dcf0ac935e85efa593ca083a
Amira Adediran <amira@Amiras-MacBook-Pro.local>
2020-03-03 15:46:32 +0100

    set up path for views in urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from django.conf.urls import url
+from .views import RoomviewAPI
 
 urlpatterns = [
     path('', include('rest_auth.urls')),
     path('registration/', include('rest_auth.registration.urls')),
+    path('adv/rooms/', RoomviewAPI.as_view()),
 ]
