f02cbc0426d499516bab1401bea20a8707c14a31
Ayomide Jones <ayormeday@gmail.com>
2020-03-05 11:55:56 +0100

    errors in sample generator resolved

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -42,42 +42,6 @@ descriptions = ["You're currently in the kitchen, you can move west to return to
 
                  ]
 
-
-
-
-
-class Room:
-    def __init__(self, id, name, description, n_to, s_to, e_to, w_to, x, y):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.n_to = None
-        self.s_to = None
-        self.e_to = None
-        self.w_to = None
-        self.x = x
-        self.y = y
-
-    # def __repr__(self):
-    #     if self.e_to is not None:
-    #         return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-    #     return f"({self.x}, {self.y})"
-    def connect_rooms(self, connecting_room, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
-
-    def get_room_in_direction(self, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        return getattr(self, f"{direction}_to")
-
-
 class World:
     def __init__(self):
         self.grid = None
@@ -171,7 +135,7 @@ class World:
 
             # Create a room in the given direction using randomization
             room = Room(id=room_count, title=random.choice(roomTitles),
-                        description=random.choice(descriptions), n_to=self.n_to,  s_to=self.s_to,  e_to=self.e_to, w_to=self.w_to, x=x, y=y)
+                        description=random.choice(descriptions), n_to=self.n_to,  s_to=self.s_to,  e_to=self.e_to, w_to=self.w_to, x=x, y= y)
            # print(room.n_to)
             room.save()
             self.list1.append(room)
@@ -188,63 +152,6 @@ class World:
             previous_room = room
             room_count += 1
 
-
-
-    # def print_rooms(self):
-    #     '''
-    #     Print the rooms in room_grid in ascii characters.
-    #     '''
-
-    #     # Add top border
-    #     str = "# " * ((3 + self.width * 5) // 2) + "\n"
-
-    #     # The console prints top to bottom but our array is arranged
-    #     # bottom to top.
-    #     #
-    #     # We reverse it so it draws in the right direction.
-    #     reverse_grid = list(self.grid) # make a copy of the list
-    #     reverse_grid.reverse()
-    #     for row in reverse_grid:
-    #         # PRINT NORTH CONNECTION ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.n_to is not None:
-    #                 str += "  |  "
-    #             else:
-    #                 str += "     "
-    #         str += "#\n"
-    #         # PRINT ROOM ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.w_to is not None:
-    #                 str += "-"
-    #             else:
-    #                 str += " "
-    #             if room is not None:
-    #                 str += f"{room.id}".zfill(3)
-    #             else:
-    #                 str += "   "
-    #             if room is not None and room.e_to is not None:
-    #                 str += "-"
-    #             else:
-    #                 str += " "
-    #         str += "#\n"
-    #         # PRINT SOUTH CONNECTION ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.s_to is not None:
-    #                 str += "  |  "
-    #             else:
-    #                 str += "     "
-    #         str += "#\n"
-
-    #     # Add bottom border
-    #     str += "# " * ((3 + self.width * 5) // 2) + "\n"
-
-    #     # Print string
-    #     print(str)
-
-
 w = World()
 num_rooms = 100
 width = 15
@@ -253,4 +160,4 @@ w.generate_rooms(width, height, num_rooms)
 # w.print_rooms()
 
 
-print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
+# print(f"\n\nWorld\n  height: {height}\n  width: {width},\n  num_rooms: {num_rooms}\n")
