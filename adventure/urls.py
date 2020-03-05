2bc3f3df43265979cd97caac213599accab5bb61
Ayomide Jones <ayormeday@gmail.com>
2020-03-05 23:32:46 +0100

    viewsets and serializer re rendered

--- adventure/urls.py
+++ adventure/urls.py
@@ -1,9 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import api
+# from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views
+from adventure.api import RoomViewSet
+
+
+router = routers.DefaultRouter()
+# register rooms with router to get into rooms api
+router.register("rooms", RoomViewSet)
 
 urlpatterns = [  
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
-    url('rooms', api.room),
+    path('', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
