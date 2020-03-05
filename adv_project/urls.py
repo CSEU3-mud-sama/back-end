c8be9c9ceccf5eaaad4f52890b580b7afa76beb8
Ayomide Jones <ayormeday@gmail.com>
2020-03-05 22:43:59 +0100

    reset urls

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,17 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from django.conf.urls import include
-from rest_framework.authtoken import views
-from adventure.api import RoomViewSet
-
-router = routers.DefaultRouter()
-# register rooms with router to get into rooms api
-router.register("rooms", RoomViewSet)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api/', include('api.urls')),
+    path('api/adv/', include('adventure.urls')),
 ]
